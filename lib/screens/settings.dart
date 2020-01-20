102585ba7a44f7da71c387623d8a6600a13fc14a
admin <kalrobbynson@gmail.com>
2020-01-20 13:28:01 +0300

    setting page design

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -4,7 +4,87 @@ class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.amber,
+      child: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            title: Text('Settings'),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Notifications',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+                    ),
+                    Card(
+                      child: ListTile(
+                        leading: Icon(Icons.notifications_off),
+                        title: Text('Work Notification'),
+                        trailing: Switch(
+                          onChanged: (bool value) {},
+                          value: false,
+                        ),
+                      ),
+                    ),
+                    Card(
+                      child: ListTile(
+                        leading: Icon(Icons.notifications_active),
+                        title: Text('Reminders'),
+                        trailing: Switch(
+                          onChanged: (bool value) {},
+                          value: true,
+                        ),
+                      ),
+                    ),
+                    Divider(),
+                    Text(
+                      'Account',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+                    ),
+                    Card(
+                      child: ListTile(
+                        leading: Icon(Icons.person),
+                        title: Text('Deactivate Account'),
+                        trailing: Switch(
+                          onChanged: (bool value) {},
+                          value: false,
+                        ),
+                      ),
+                    ),
+                    Card(
+                      child: ListTile(
+                        leading: Icon(Icons.delete),
+                        title: Text('Delete Account'),
+                      ),
+                    ),
+                    Divider(),
+                    SizedBox(
+                      height: 100,
+                    ),
+                    Text(
+                      'Logout',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+                    ),
+                    Card(
+                      child: ListTile(
+                        leading: Icon(Icons.exit_to_app),
+                        title: Text('Logout'),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ]),
+          )
+        ],
+      ),
     );
   }
 }
